4f4b482d4cc06e6b0a6dfc482d8964cb0ead37a2
mohammedAdelAv <medoadel029130@gmail.com>
2026-02-03 15:54:58 +0200

    finsh

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Projects } from '../models/projects';
+import { Message } from '../models/message';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,7 @@ import { Projects } from '../models/projects';
 export class DataService {
 
   private dataUrl = 'http://localhost:3005/projects';
+  private messagesUrl = 'http://localhost:3005/massages';
 
   constructor(private http: HttpClient) { }
 
@@ -17,4 +20,12 @@ export class DataService {
   get(): Observable<Projects> {
     return this.http.get<Projects>(this.dataUrl);
   }
+
+  //---------------------------------------------------------------------
+
+  // post message form data
+  post(objForm: Message): Observable<Message> {
+    return this.http.post<Message>(this.messagesUrl, objForm);
+  }
+
 }
--- src/app/features/single-page/single-page.ts
+++ src/app/features/single-page/single-page.ts
@@ -1,21 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
 import { Projects } from '../../core/models/projects';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Message } from '../../core/models/message';
 
 @Component({
   selector: 'app-single-page',
-  imports: [ CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './single-page.html',
   styleUrl: './single-page.css',
 })
-export class SinglePage {
+export class SinglePage implements OnInit {
 
+  // 1
+  massageForm!: FormGroup;
 
   projectsData: Projects[] = [];
 
-  constructor(private serv: DataService) {
+  constructor(private serv: DataService, private fb: FormBuilder
+  ) {
     this.showData();
+  }
+
+  ngOnInit(): void {
+    // 3
+    this.createForm();
+  }
+
+  // 4
+  get controls() {
+    return this.massageForm.controls;
+  }
+
+  // 2
+  createForm() {
+    this.massageForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
 
   //show data
@@ -24,4 +49,10 @@ export class SinglePage {
       this.projectsData = res;
     });
   }
+
+  submit(massageForm: any) {
+    this.serv.post(massageForm.value).subscribe((res: any) => { });
+  }
+
+
 }
